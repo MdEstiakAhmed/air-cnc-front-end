9fc5564ff8e001029fcddeca599822269e5acf25
nibir <biplob.asanibir@gmail.com>
2020-06-05 19:26:36 +0600

    add sign in page

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RegistrationForm.scss'
 import { Form, Button } from 'react-bootstrap';
-import {Input} from "@material-ui/core";
 import { useForm, Controller } from "react-hook-form";
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/bootstrap.css';
 
 const RegistrationForm = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, control, handleSubmit } = useForm();
     const onSubmit = data => console.log(data);
+
     return (
         <div>
             <h2 className="bookingForm-title mb-4 text-center">Sign In </h2>
@@ -17,6 +19,17 @@ const RegistrationForm = () => {
                     </div>
                     <div className="login-input mb-3">
                         <Form.Control className="border-0 shadow-none" type="email" name="email" defaultValue="" ref={register} placeholder="Enter email" />
+                    </div>
+                    <div className="login-input mb-3">
+                        <Controller 
+                        as={
+                            <PhoneInput
+                            country={"bd"}
+                            inputClass="shadow-none border-0"
+                            />
+                        } 
+                        
+                        name="phone" control={control} defaultValue="" />
                     </div>
                 </Form.Group>
                 <Button  className="search-btn gradient-button shadow-none border-0 p-3" type="submit">
