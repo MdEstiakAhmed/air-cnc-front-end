1a9682906b588b8684c7089861c2bc1c423d604e
nibir <biplob.asanibir@gmail.com>
2020-06-07 23:18:01 +0600

    fixed Experience Error

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import MainApp from '../../app/MainApp';
-import { Container, Row, Col } from 'react-bootstrap';
-import BookingForm from '../../components/BookingForm/BookingForm';
-import HouseListArea from '../../components/HouseListArea/HouseListArea';
-import ExperienceListArea from '../../components/ExperienceListArea/ExperienceListArea';
+import React from "react";
+import MainApp from "../../app/MainApp";
+import { Container, Row, Col } from "react-bootstrap";
+import BookingForm from "../../components/BookingForm/BookingForm";
+import HouseListArea from "../../components/HouseListArea/HouseListArea";
+import ExperienceListArea from "../../components/ExperienceListArea/ExperienceListArea";
 
 const Home = () => {
-    return (
-        <div>
-            <MainApp>
-                <Container>
-                    <Row className="mt-5">
-                        <Col sm={12} md={12} lg={4} xl={4} >
-                            <BookingForm/>
-                        </Col>
-                        <Col sm={12} md={12} lg={8} xl={8} >
-                            {/* <ExperienceListArea/> */}
-                            <HouseListArea/>
-                        </Col>
-                    </Row>
-                </Container>
-            </MainApp>
-        </div>
-    );
+  return (
+    <div>
+      <MainApp>
+        <Container>
+          <Row className="mt-5">
+            <Col sm={12} md={12} lg={4} xl={4}>
+              <BookingForm />
+            </Col>
+            <Col sm={12} md={12} lg={8} xl={8}>
+              <ExperienceListArea />
+              <HouseListArea />
+            </Col>
+          </Row>
+        </Container>
+      </MainApp>
+    </div>
+  );
 };
 
 export default Home;
